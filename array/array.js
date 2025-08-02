158068d60274dfc5282935acb7671247590a9d5e
priyoitush paul <priyotushpaul40@gmail.com>
2025-08-02 10:35:59 +0600

    parctice js

---
+++ array/array.js
@@ -0,0 +1,20 @@
+//** array learning **
+
+// push(), pop(), shift(), unshift();
+let result;
+let num1 = [400, 500, 600];
+let numbers = [10, 20, 30, 40, 50];
+
+// numbers.push(100);
+// numbers.pop();
+
+// numbers.shift();
+// numbers.unshift(100, 200, 300, 400);
+
+// result = numbers.concat(num1);
+
+// result = numbers.slice(0, 2 + 1); - crate new array but cant modify original array
+// result = numbers.splice(0, 3, 100, 200, 500); - crate new array and modify original array
+
+result = console.log(result);
+console.log(numbers);
