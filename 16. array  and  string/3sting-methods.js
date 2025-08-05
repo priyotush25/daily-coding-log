0908861c31e10098b331c023717a5de45e516b19
priyoitush paul <priyotushpaul40@gmail.com>
2025-08-05 13:10:19 +0600

    sting revese

--- 16. array  and  string/3sting-methods.js
+++ 16. array  and  string/3sting-methods.js
@@ -5,3 +5,20 @@
 // join()
 // concat()
 // includes()
+
+let country = "I Love Bangladesh";
+let expression = "always ";
+
+// slice() methods
+console.log(country.slice(0, 2));
+
+// split()
+
+let arr = country.split(" ");
+console.log(arr);
+
+// join()
+console.log(arr.join(" "));
+
+// concat()
+console.log(expression.concat(country));
--- 16. array  and  string/4revese.js
+++ 16. array  and  string/4revese.js
@@ -3,3 +3,27 @@
 // for loop use - user reverse string
 // for of loop - use reverse string
 // split() , reverse(), join() - use reverse string
+
+let profession = "Web Developer";
+
+// use for loop
+let reverse = "";
+
+for (var i = 0; i < profession.length; i++) {
+  reverse = profession[i] + reverse;
+}
+console.log(reverse);
+
+// use for of
+let emptyStr = "";
+for (let word of profession) {
+  emptyStr = word + emptyStr;
+}
+
+console.log(emptyStr);
+
+// use methods
+
+let splitArry = profession.split("").reverse().join("");
+
+console.log(splitArry);
