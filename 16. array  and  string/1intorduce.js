735fe0b9cfdcb2a0b48a906a0ba6e9e4bf42b210
priyoitush paul <priyotushpaul40@gmail.com>
2025-08-05 12:21:23 +0600

    string and array

--- 16. array  and  string/1intorduce.js
+++ 16. array  and  string/1intorduce.js
@@ -4,3 +4,28 @@
 // length
 // index
 // mutable and immutable
+
+let name = "priyotush Paul";
+let district = `Habiganj`;
+let country = new String("Bangladesh");
+
+console.log(name);
+console.log(district);
+console.log(country);
+
+console.log(name.length);
+
+console.log(name[0]);
+
+// array - mutable
+let fruits = ["apple", "banana", "mango", "cucumber"];
+
+console.log((fruits[0] = "jackfruit"));
+console.log(fruits);
+
+// string - immutable
+let sentence = "Hello world!";
+
+sentence[2] = "B";
+
+console.log(sentence);
--- 16. array  and  string/7object-keay-values.js
+++ 16. array  and  string/7object-keay-values.js
@@ -11,11 +11,19 @@ const computer = {
 // property
 const keys = Object.keys(computer);
 console.log(keys);
+console.log(keys[0]);
+console.log(keys[1]);
 
 // value
 const value = Object.values(computer);
 console.log(value);
+console.log(value[0]);
+console.log(value[4]);
 
 // delete property
 delete computer.monitor;
 console.log(computer);
+
+// delete property value
+console.log("delete value : ", delete value[0]);
+console.log(value);
