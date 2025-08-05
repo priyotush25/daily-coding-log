ab35defd29d6141426d2b28af442d6229c72cfba
priyoitush paul <priyotushpaul40@gmail.com>
2025-08-05 12:27:29 +0600

    string methods

--- 16. array  and  string/2comparison.js
+++ 16. array  and  string/2comparison.js
@@ -3,3 +3,15 @@
 // toLowerCase()
 // toUpperCase()
 // trim()
+
+let readSubject = "English";
+let examSubject = "english  ";
+
+let subject1 = readSubject.toLocaleLowerCase().trim();
+let subject2 = examSubject.toLocaleLowerCase().trim();
+
+if (subject1 == subject2) {
+  console.log("best exam preparation");
+} else {
+  console.log("bad exam preparation");
+}
