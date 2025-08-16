fe2e5b386570a683d8bc8a4ca11b7588490f57e5
Priyotush <priyotushpaul40@gmail.com>
2025-08-16 14:54:34 +0600

    js math function

---
+++ 20. Codding Problem/5math-methods.js
@@ -0,0 +1,33 @@
+// max
+let max = Math.max(10, 20, 50, 45, 76, 89, 30);
+console.log(max);
+
+// min
+let min = Math.min(10, 20, 50, 45, 76, 89, 30);
+console.log(min);
+
+// PI
+console.log(Math.PI);
+
+// abs
+console.log(Math.abs(-5));
+console.log(Math.abs(5 - 10));
+
+// round
+console.log(Math.round(4.2));
+console.log(Math.round(4.6));
+
+// floor
+console.log(Math.floor(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.floor(-6));
+
+// ceil
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(4.6));
+console.log(Math.ceil(-6));
+
+// random
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.round(Math.random() * 10));
