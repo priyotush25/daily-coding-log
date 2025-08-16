4eea75cb9d10800a2dcc4620d52ccfdb222d3ff2
Priyotush <priyotushpaul40@gmail.com>
2025-08-16 14:06:25 +0600

    js dom

---
+++ dom-project/index.js
@@ -0,0 +1,11 @@
+let doc = document;
+
+let light = doc.querySelector("#light");
+
+let style = light.style;
+style.width = "200px";
+style.height = "200px";
+style.border = "2xp solid black";
+style.borderRadius = "100%";
+
+console.log(light);
