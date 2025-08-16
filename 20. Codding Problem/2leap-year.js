e61fcc6b021d814393644dafe6391f01dcd984ee
Priyotush <priyotushpaul40@gmail.com>
2025-08-16 14:16:06 +0600

    leap year

--- 20. Codding Problem/2leap-year.js
+++ 20. Codding Problem/2leap-year.js
@@ -3,9 +3,14 @@
 function leapYear(year) {
   if (year % 4 == 0 && year % 100 !== 0) {
     return true;
+  } else if (year % 400 == 0) {
+    return true;
   } else {
     return false;
   }
 }
 
 console.log(leapYear(2025));
+console.log(leapYear(2021));
+console.log(leapYear(1900));
+console.log(leapYear(2052));
