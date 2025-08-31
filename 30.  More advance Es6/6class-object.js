fb504b4d45235942b754942a05e107fe14ed4c11
Priyotush <priyotushpaul40@gmail.com>
2025-08-31 22:04:52 +0600

    class explore

---
+++ 30.  More advance Es6/6class-object.js
@@ -0,0 +1,13 @@
+// class and object
+
+class Player {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const player1 = new Player("Priyotush", 24);
+const player2 = new Player("sondip", 23);
+console.log(player1);
+console.log(player2);
