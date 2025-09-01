2e5962d907d9c0ec60b83ae7ec5bea50478b88b5
Priyotush <priyotushpaul40@gmail.com>
2025-09-01 18:46:58 +0600

    undefind explore

---
+++ 31. More Es6 explore/1null-undefind.js
@@ -0,0 +1,27 @@
+let name;
+
+console.log(name);
+
+// function
+function add(a) {
+  console.log(a);
+}
+
+add();
+
+// function return
+function hello(name) {
+  return;
+}
+
+console.log(hello());
+
+// object
+const phone = {
+  brand: "iphone",
+  price: 45000,
+};
+
+console.log(phone.color);
+
+console.log("type of undefined : ", typeof undefined);
