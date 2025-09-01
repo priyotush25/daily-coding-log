df8a6c9d934d6ef2e26135e2085537c663248ea3
Priyotush <priyotushpaul40@gmail.com>
2025-09-01 19:16:03 +0600

    compare value

---
+++ 31. More Es6 explore/3tripple-equal.js
@@ -0,0 +1,21 @@
+console.log(1 === 2);
+console.log(false === 0);
+console.log(null === undefined);
+
+console.log([] === "");
+
+console.log(2 === 2);
+console.log(2 == "2");
+
+console.log(true === 1);
+console.log(true == 1);
+
+console.log(false === 0);
+console.log(false == 0);
+
+console.log(NaN === NaN);
+console.log(NaN == NaN);
+
+console.log([5] === "5");
+
+console.log([] === []);
